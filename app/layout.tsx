2f58c7b0a709c8ad786fe93ae2d1b96349d50520
Selina Lavery <selinalavery@Selinas-MacBook-Pro.local>
2025-02-15 23:06:43 +0000

    debugging to fix hydration warning

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,10 @@
-import '@/app/ui/global.css';
-import { inter } from '@/app/ui/fonts'; // Import the Inter font from the fonts module
+import "@/app/ui/global.css";
+import { inter } from "@/app/ui/fonts";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased`} >{children}</body> {/* By adding Inter to the <body> element, the font will be applied throughout your application */}
+    <html lang="en" >
+      <body>{children}</body>
     </html>
   );
 }
