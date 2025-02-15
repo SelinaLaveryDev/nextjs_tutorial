88734b3e2f20265750ec3aa4a72aa5861eb81d16
Selina Lavery <selinalavery@Selinas-MacBook-Pro.local>
2025-02-15 19:40:48 +0000

    added customer folder and page.tsx, and invoices folder and page.tsx. Then created a layout.tsx file and added the sidebar component

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+// This is how you can create different pages in Next.js: 
+// create a new route segment using a folder, and add a page file inside it.
+// The page file is a React component that exports a default function.
+// This function returns the JSX that will be rendered as the page content.
+// Can view this page at http://localhost:3000/dashboard/customers
+
+export default function Page() {
+    return <p>Customers Page</p>;
+  }
---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,15 @@
+// Here, we are importing the <SideNav /> component into your layout.tsx file. 
+// Any components imported into this layout.tsx file will be part of the layout.
+
+import SideNav from '@/app/ui/dashboard/sidenav';
+ 
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      <div className="w-full flex-none md:w-64">
+        <SideNav />
+      </div>
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+    </div>
+  );
+}
