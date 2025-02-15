4fbfab4310eb02b77ba6865e30202ec8718d7ed4
Selina Lavery <selinalavery@Selinas-MacBook-Pro.local>
2025-02-15 23:20:12 +0000

    debugging to fix hydration warning - font path

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,12 +1,4 @@
-// the fonts that will be used throughout the application
-// Import the Inter font from the next/font/google module - this will be the primary font.
-import { Inter } from 'next/font/google';
-// Import the Lusitana font from the next/font/google module - this will be the secondary font.
-import { Lusitana } from 'next/font/google';
+import { Inter, Lusitana } from "next/font/google";
 
-
-export const inter = Inter({ subsets: ['latin'] }); // specify the latin subset for the Inter font
-export const lusitana = Lusitana({ // specify the font weights and subsets for the Lusitana font
-    weight: ["400", "700"], // specify the font weights for the Lusitana font
-    subsets: ['latin'], // specify the latin subset for the Lusitana font
-});
+export const inter = Inter({ subsets: ["latin"] });
+export const lusitana = Lusitana({ weight: ["400", "700"], subsets: ["latin"] });
