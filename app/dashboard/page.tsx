19ccec6f17fd376452795ffda7532774ae3bd8cc
Selina Lavery <selinalavery@Selinas-MacBook-Pro.local>
2025-02-16 16:06:52 +0000

    updated imports to fix the revenue bug

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,10 +6,11 @@
 
 import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '../ui/dashboard/revenue-chart';
-import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
-import { lusitana } from '@/app/ui/fonts';
+import LatestInvoices from '../ui/dashboard/latest-invoices';
+import { lusitana } from '../ui/fonts';
+import { revenue } from '../lib/placeholder-data';
  
-export default async function Page( ) {
+export default async function Page() {
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -26,7 +27,7 @@ export default async function Page( ) {
         /> */}
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue}  />
+        <RevenueChart revenue={revenue}  />  {/* revenue is an array of objects. here we are passing it as a prop to RevenueChart component */}
         {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
       </div>
     </main>
