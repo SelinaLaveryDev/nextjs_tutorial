d23f21569e4b0c97d8ccc0a714e4a00c6ef54e3e
Selina Lavery <selina118@hotmail.com>
2025-03-03 15:03:29 +0000

    starting code for chapter 11 'adding search and pagination'

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,9 +1,33 @@
-// This is how you can create different pages in Next.js: 
+// This is how you can create different pages in Next.js:
 // create a new route segment using a folder, and add a page file inside it.
 // The page file is a React component that exports a default function.
 // This function returns the JSX that will be rendered as the page content.
 // Can view this page at http://localhost:3000/dashboard/invoices
 
-export default function Page() {
-    return <p>Invoices Page</p>;
-  }
+import Pagination from "@/app/ui/invoices/pagination";
+import Search from "@/app/ui/search";
+import Table from "@/app/ui/invoices/table";
+import { CreateInvoice } from "@/app/ui/invoices/buttons";
+import { lusitana } from "@/app/ui/fonts";
+import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
+import { Suspense } from "react";
+
+export default async function Page() {
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search invoices..." />
+        <CreateInvoice />
+      </div>
+      {/*  <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <Table query={query} currentPage={currentPage} />
+      </Suspense> */}
+      <div className="mt-5 flex w-full justify-center">
+        {/* <Pagination totalPages={totalPages} /> */}
+      </div>
+    </div>
+  );
+}
