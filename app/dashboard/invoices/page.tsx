e7d1889d0801933666ad627d58bc29b4df1f041d
Selina Lavery <selina118@hotmail.com>
2025-03-04 11:07:12 +0000

    In /dashboard/invoices/page.tsx, import a new function called fetchInvoicesPages and pass the query from searchParams as an argument

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -11,6 +11,7 @@ import { CreateInvoice } from "@/app/ui/invoices/buttons";
 import { lusitana } from "@/app/ui/fonts";
 import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
 import { Suspense } from "react";
+import { fetchInvoicesPages } from "@/app/lib/data";
 
 export default async function Page(props: {
   searchParams?: Promise<{
@@ -21,6 +22,7 @@ export default async function Page(props: {
   const searchParams = await props.searchParams;
   const query = searchParams?.query || "";
   const currentPage = Number(searchParams?.page) || 1;
+  const totalPages = await fetchInvoicesPages(query);
 
   return (
     <div className="w-full">
